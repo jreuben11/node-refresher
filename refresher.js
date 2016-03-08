825ffd3e9d1d7ba796c758f6596fded9c4d1d688
joshr <jreuben111@gmail.com>
2016-03-08 07:01:40 +0200

    added package.json

--- refresher.js
+++ refresher.js
@@ -1,7 +1,7 @@
  var http = require("http");
  http.createServer(function(request, response) {
-   console.log("REQUEST", request);
+   //console.log("REQUEST", request);
    response.writeHead(200, {"Content-Type": "text/html"});
    response.end("Refresher");
-   console.log("RESPONSE", response);
+   //console.log("RESPONSE", response);
 }).listen(8080);
