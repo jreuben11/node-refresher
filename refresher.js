41fe1d8a579126da4555b22f603f32a4fdfe0592
joshr <jreuben111@gmail.com>
2016-03-09 07:04:12 +0200

    non-blocking file access

--- refresher.js
+++ refresher.js
@@ -1,7 +1,22 @@
  var http = require("http");
+ var fs = require("fs");
  http.createServer(function(request, response) {
-   //console.log("REQUEST", request);
-   response.writeHead(200, {"Content-Type": "text/html"});
+   console.log(request.url);
+   response.writeHead(200, {
+     "Content-Type": "text/html"
+   });
+
+   // BLOCKING:
+   //var data = fs.readFileSync('package.json');
+   //console.log(data.toString());
+
+   // NON-BLOCKING:
+   fs.readFile('package.json', function(err, data) {
+     if (err) return console.error(err);
+     console.log("readfile finished");
+   });
+   console.log("readfile called");
+
    response.end("Refresher");
    //console.log("RESPONSE", response);
-}).listen(8080);
+ }).listen(8080);
