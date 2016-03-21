66cab6aa9aaf971476298cd2d189752b4e0aed1b
joshr <jreuben111@gmail.com>
2016-03-21 07:01:02 +0200

    simple node static server

--- fileReader.js
+++ fileReader.js
@@ -1,10 +1,23 @@
 var fs = require("fs");
 
-fs.readFile('fileReader.js', function (err, data) {
+fs.readFile('input.txt', function (err, data) {
    if (err){
       console.log(err.stack);
       return;
    }
    console.log(data.toString());
+});
+
+console.log("Going to get file info!");
+fs.stat('input.txt', function (err, stats) {
+   if (err) {
+       return console.error(err);
+   }
+   console.log(stats);
+   console.log("Got file info successfully!");
+
+   // Check file type
+   console.log("isFile ? " + stats.isFile());
+   console.log("isDirectory ? " + stats.isDirectory());
 });
 console.log("Program Ended");
