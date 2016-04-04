73b4b301facb91e72f749d546e61c3aa5649252e
joshr <jreuben111@gmail.com>
2016-04-04 06:12:33 +0300

    node-express, multer

--- express-server.js
+++ express-server.js
@@ -1,26 +1,83 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var fs = require("fs");
+var multer  = require('multer');
+var uploads = multer({ dest: '/tmp/'});
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 //app.use(express.static('static'));
 
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "express_index.html" );
+})
+
+app.get('/process_get', function (req, res) {
+
+   // Prepare output in JSON format
+   response = {
+       first_name:req.query.first_name,
+       last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+
+   // Prepare output in JSON format
+   response = {
+       first_name:req.body.first_name,
+       last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+app.post('/file_upload', function (req, res) {
+
+   console.log(req.files.file.name);
+   console.log(req.files.file.path);
+   console.log(req.files.file.type);
+
+   var file = __dirname + "/" + req.files.file.name;
+   fs.readFile( req.files.file.path, function (err, data) {
+        fs.writeFile(file, data, function (err) {
+         if( err ){
+              console.log( err );
+         }else{
+               response = {
+                   message:'File uploaded successfully',
+                   filename:req.files.file.name
+              };
+          }
+          console.log( response );
+          res.end( JSON.stringify( response ) );
+       });
+   });
+})
+
+
 // This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
    console.log("Got a GET request for the homepage");
-   res.send('Hello GET');
+   res.send('Hello GET\n');
 })
 
 
 // This responds a POST request for the homepage
 app.post('/', function (req, res) {
    console.log("Got a POST request for the homepage");
-   res.send('Hello POST');
+   res.send('Hello POST\n');
 })
 
 // This responds a DELETE request for the /del_user page.
 app.delete('/del_user', function (req, res) {
    console.log("Got a DELETE request for /del_user");
-   res.send('Hello DELETE');
+   res.send('Hello DELETE\n');
 })
 
 // This responds a GET request for the /list_user page.
